a365c08002b531fa66065357aee8a9ef2182aa95
yeisonagm <garciamonsefu@gmail.com>
2024-02-15 21:24:08 -0500

    Crea la estructura del proyecto rentacar

---
+++ src/main/java/ygm/rentacargarcia/RentacargarciaApplication.java
@@ -0,0 +1,13 @@
+package ygm.rentacargarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentacargarciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentacargarciaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ygm/rentacargarcia/RentacargarciaApplicationTests.java
@@ -0,0 +1,13 @@
+package ygm.rentacargarcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacargarciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
